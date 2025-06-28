5e21c5cebfe5605a2d82068460775ba139b2b040
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2025-06-28 22:45:40 +0300

    feat: init

---
+++ admin/script.js
@@ -0,0 +1,79 @@
+const names = [
+  { id: "0", names: "Дорогие друзья" },
+  { id: "1", names: "Дорогие Антон и Ксения" },
+  { id: "2", names: "Дорогие Анастасия и Пётр" },
+  { id: "3", names: "Дорогие Светлана и Дмитрий" },
+  { id: "4", names: "Дорогой друг Слава" },
+  { id: "5", names: "Дорогой друг Никита" },
+  { id: "6", names: "Дорогая Мария" },
+  { id: "7", names: "Дорогая Кристина" }
+];
+
+const baseURL = location.href.replace('admin/', ''); //location.origin
+const container = document.getElementById('container');
+
+names.forEach((name, index) => {
+  const nameElem = document.createElement('div');
+  nameElem.classList.add('name');
+  const titleNameElem = document.createElement('p');
+  titleNameElem.classList.add('name__title');
+  titleNameElem.innerHTML = name.names;
+  const linkNameElem = document.createElement('a');
+  linkNameElem.classList.add('name__link');
+
+  const link = `${baseURL}?id=${name.id}`;
+  linkNameElem.href = link;
+  linkNameElem.innerHTML = link;
+  linkNameElem.target = '_blank';
+
+  const btnCopyLink = document.createElement('button');
+  btnCopyLink.innerHTML = 'copy';
+  btnCopyLink.classList.add('name__btn-copy');
+  btnCopyLink.dataset.link = link;
+
+  const btnShareViber = document.createElement('a');
+  btnShareViber.classList.add('name__btn-share-viber');
+  btnShareViber.href = 'viber://forward?text=' + link;
+  btnShareViber.target = '_blank';
+
+  const btnShareTelegram = document.createElement('a');
+  btnShareTelegram.classList.add('name__btn-share-telegram');
+  btnShareTelegram.href = `https://telegram.me/share/url?url=${link}&amp;text=${link}`;
+  btnShareTelegram.target = '_blank';
+
+  const linkWrapper = document.createElement('div');
+  linkWrapper.classList.add('name__link-wrapper');
+
+  linkWrapper.append(
+    linkNameElem,
+    btnCopyLink,
+    btnShareViber,
+    btnShareTelegram
+  );
+
+  nameElem.append(titleNameElem, linkWrapper);
+
+  container.append(nameElem);
+});
+
+container.addEventListener('click', (e) => {
+  const btnCopy = e.target;
+
+  if (btnCopy.classList.contains('name__btn-copy')) {
+    const linkUrl = btnCopy.dataset.link;
+    console.log(linkUrl);
+    navigator.clipboard
+      .writeText(linkUrl)
+      .then(() => {
+        console.log('Text copied to clipboard ' + linkUrl);
+        btnCopy.innerHTML = 'copied';
+
+        setTimeout(() => {
+          btnCopy.innerHTML = 'copy';
+        }, 5000);
+      })
+      .catch((err) => {
+        console.error('Error in copying text: ', err);
+      });
+  }
+});
